3658d8b4a6f4ce5a63efcb9e28e1a41d1810029f
shashank-kumar-saxena <69252042+shashank-kumar-saxena@users.noreply.github.com>
2021-05-24 07:01:34 -0700

    Add files via upload

---
+++ starbuks.js
@@ -0,0 +1,22 @@
+function show()
+{
+   var nav=document.getElementById("navigation");
+    nav.classList.toggle("nav2");
+    var body=document.querySelector(".Light");
+    body.classList.toggle("blur");
+}
+function dark()
+{
+    var body=document.querySelector(".Light");
+    body.classList.toggle("Dark");
+    var body=document.querySelectorAll(".white");
+    for(var i=0;i<body.length;i++)
+    {
+    body[i].classList.toggle("Black");
+    }
+    switchcolor.classList.toggle("activedark");
+}
+var switchcolor=document.getElementById("btn");
+switchcolor.addEventListener('click',dark,false);
+var dropmenu=document.getElementById("menu");
+dropmenu.addEventListener("click",show,false);
